831b683b3ff123cf86c171840f6aa5c59b5d1cf1
RxbPythonDev <rxbpythondev@gmail.com>
2023-03-31 19:40:05 -0300

    Códigos praticados durante a aula ao vivo da semana 3

---
+++ Semana 3/exemplo2.py
@@ -0,0 +1,24 @@
+#Importando TUDO
+#import funcoes_compartilhadas
+#funcoes_compartilhadas.soma(2,5,6,9,5,1,5,9,6,8,5)
+
+#Importando apenas o que eu desejo
+"""from funcoes_compartilhadas import soma, cadastro_usuario
+soma(2,5,6)
+cadastro_usuario()"""
+
+"""
+def teste(**valores):
+    print(valores["numero"])
+teste(numero = 9)
+"""
+
+"""def par_impar(numero):
+    if numero % 2 == 0:
+        return "Par"
+    else:
+        return "Impar"
+
+numero = int(input())
+resposta = par_impar(numero)
+print(resposta)"""
